934578b2385c1c71c7908cd2a0711a4385835d72
Daniel <danielsalazarpares@gmail.com>
2026-04-22 22:40:05 +0200

    feat: now able to use abbreviations to find address

--- src/main.c
+++ src/main.c
@@ -24,7 +24,77 @@ typedef struct House{
 
 } House;
 
+typedef enum {
+    TYPE_UNKNOWN = 0,
+    TYPE_STREET,
+    TYPE_AVENUE,
+    TYPE_PASSAGE
+} StreetType;
 
+static void ltrim(char *s) {
+    while (*s == ' ') {
+        memmove(s, s + 1, strlen(s));
+    }
+}
+
+static StreetType get_type_and_strip_prefix(char *s) {
+    ltrim(s);
+
+    if (strncmp(s, "carrer ", 7) == 0 ||
+        strncmp(s, "calle ", 6)  == 0 ||
+        strncmp(s, "c/ ", 3)     == 0 ||
+        strncmp(s, "c. ", 3)     == 0) {
+        if (strncmp(s, "carrer ", 7) == 0){
+          memmove(s, s + 7, strlen(s + 7) + 1);
+        }      
+        else if (strncmp(s, "calle ", 6) == 0) {
+          memmove(s, s + 6, strlen(s + 6) + 1);
+        }
+        else {
+          memmove(s, s + 3, strlen(s + 3) + 1);
+        }
+        ltrim(s);
+        return TYPE_STREET;
+    }
+
+    if (strncmp(s, "avinguda ", 9) == 0 ||
+        strncmp(s, "avenida ", 8)  == 0 ||
+        strncmp(s, "av. ", 4)      == 0 ||
+        strncmp(s, "av ", 3)       == 0) {
+        if (strncmp(s, "avinguda ", 9) == 0) {
+          memmove(s, s + 9, strlen(s + 9) + 1);
+        }
+        else if (strncmp(s, "avenida ", 8) == 0) {
+          memmove(s, s + 8, strlen(s + 8) + 1);
+        }
+        else if (strncmp(s, "av. ", 4) == 0) {
+          memmove(s, s + 4, strlen(s + 4) + 1);
+        }
+        else {
+          memmove(s, s + 3, strlen(s + 3) + 1);
+        }
+        ltrim(s);
+        return TYPE_AVENUE;
+    }
+
+    if (strncmp(s, "passatge ", 9) == 0 ||
+        strncmp(s, "pasaje ", 7)   == 0 ||
+        strncmp(s, "psg. ", 5)     == 0) {
+        if (strncmp(s, "passatge ", 9) == 0) {
+          memmove(s, s + 9, strlen(s + 9) + 1);
+        }
+        else if (strncmp(s, "pasaje ", 7) == 0) {
+          memmove(s, s + 7, strlen(s + 7) + 1);
+        }
+        else {
+          memmove(s, s + 5, strlen(s + 5) + 1);
+        }
+        ltrim(s);
+        return TYPE_PASSAGE;
+    }
+
+    return TYPE_UNKNOWN;
+}
 
 char* get_map_name(void);
 int get_option(void);
@@ -149,50 +219,73 @@ House* find_coordinates(char* street, int number, char* mapName){
   
   House* house = malloc(sizeof(House));
   if (house == NULL) return NULL;
+
+  char streetNorm[STREET_SIZE];
+  strncpy(streetNorm, street, sizeof(streetNorm) - 1);
+  streetNorm[sizeof(streetNorm) - 1] = '\0';
+
+  StreetType userType = get_type_and_strip_prefix(streetNorm);
+
   int found = 0;
   char filename[64];
   sprintf(filename, "../maps/%s/houses.txt", mapName);
-  //printf("Filename: %s\n", filename);
 
   int *numeros = NULL;
   int size = 0;
   int capacity = 0;
 
   FILE *file = fopen(filename, "r");  
-
   if(file == NULL) {
     free(house);
     return NULL;
   }
   
-  while(fscanf(file, "%128[^,],%d,%f,%f", house->street, &house->number, &house->latitude, &house->longitude) == 4){
-    //printf("Buscando: '%s' %d \n Archivo: '%s' %d\n", street, number, house->street, house->number);
-    for(int i = 0; i < strlen(house->street); i++){
+  while (fscanf(file, " %127[^,],%d,%f,%f",
+              house->street, &house->number,
+              &house->latitude, &house->longitude) == 4) {
+                
+    for (int i = 0; i < strlen(house->street); i++) {
       house->street[i] = tolower((unsigned char)house->street[i]);
     }
-    //printf("%s", street);
-    if(strstr(house->street, street) != NULL) {
+
+    char houseNorm[STREET_SIZE];
+    strncpy(houseNorm, house->street, sizeof(houseNorm) - 1);
+    houseNorm[sizeof(houseNorm) - 1] = '\0';
+
+    StreetType lineType = get_type_and_strip_prefix(houseNorm);
+
+    if (userType != TYPE_UNKNOWN && userType != lineType) {
+      
+      continue;
+    }
+
+    if (strstr(houseNorm, streetNorm) != NULL) {
       found = 1;
+
       if (house->number == number) {
         printf("Encontrado: '%s' %d \n", house->street, house->number);
         fclose(file);
+        free(numeros);
         return house;
       }
+
       if (size >= capacity) {
-        capacity = capacity ? capacity * 2 : 4; // If capacity has a value != 0, it doubles the capacidad, else capacity = 4
+        capacity = capacity ? capacity * 2 : 4;
         numeros = realloc(numeros, capacity * sizeof(int));
       }
-      numeros[size++] = house->number; // Stores numbers of the addres if the introduced one is not found.
-    }
-  }
-  
-  if (found) {
-    printf("Indtroduced number was not found, avaliavle ones are the following: ");
-    for (int i = 0; i < size; i++) {
-      printf("%d, ", numeros[i]);
+      numeros[size++] = house->number;
     }
   }
 
+  if (found) {
+    printf("Introduced number was not found, available ones are the following: ");
+    for (int i = 0; i < size; i++) {
+      printf("%d, ", numeros[i]);
+    }
+    printf("\n");
+  }
+
+  free(numeros);
   fclose(file);
   free(house);
   return NULL;
