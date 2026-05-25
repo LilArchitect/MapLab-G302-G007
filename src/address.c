c2822f32724ff19c46e418d0072b48d76fb743a0
Dani <danielsalazarpares@gmail.com>
2026-05-25 11:22:23 +0200

    fix: added error handeling for incorrect format in houses.txt and places.txt

--- src/address.c
+++ src/address.c
@@ -13,11 +13,10 @@
 
 #define MAX_SUGGESTIONS 5
 #define MAX_DISTANCE 5
-//static void ltrim(char *s);
-//static StreetType get_type_and_strip_prefix(char *s);
-//void address(char* mapName);
-//House* find_coordinates(char* street, int number, char* mapName);
-
+// static void ltrim(char *s);
+// static StreetType get_type_and_strip_prefix(char *s);
+// void address(char* mapName);
+// House* find_coordinates(char* street, int number, char* mapName);
 
 /*
 void address(char* mapName){
@@ -40,11 +39,11 @@ void address(char* mapName){
   while(result == NULL);
 
   printf("Found at: ( %lf, %lf )\n", result->latitude, result->longitude);
-  
+
 }
 
 House* find_coordinates(char* street, int number, char* mapName){
-  
+
   House* house = malloc(sizeof(House));
   if (house == NULL) return NULL;
 
@@ -62,16 +61,16 @@ House* find_coordinates(char* street, int number, char* mapName){
   int size = 0;
   int capacity = 0;
 
-  FILE *file = fopen(filename, "r");  
+  FILE *file = fopen(filename, "r");
   if(file == NULL) {
     free(house);
     return NULL;
   }
-  
+
   while (fscanf(file, " %127[^,],%d,%f,%f",
               house->street, &house->number,
               &house->latitude, &house->longitude) == 4) {
-                
+
     for (size_t i = 0; i < strlen(house->street); i++) {
       house->street[i] = tolower((unsigned char)house->street[i]);
     }
@@ -83,7 +82,7 @@ House* find_coordinates(char* street, int number, char* mapName){
     StreetType lineType = get_type_and_strip_prefix(houseNorm);
 
     if (userType != TYPE_UNKNOWN && userType != lineType) {
-      
+
       continue;
     }
 
@@ -91,7 +90,7 @@ House* find_coordinates(char* street, int number, char* mapName){
       found = 1;
 
       if (house->number == number) {
-        
+
         fclose(file);
         free(numeros);
         return house;
@@ -104,11 +103,11 @@ House* find_coordinates(char* street, int number, char* mapName){
       numeros[size++] = house->number; // Stores numbers of the addres if the introduced one is not found.
     }
   }
-  
+
   if (found) {
     int pos_num = 0;
     bubble_sort(numeros, size);
-    
+
     int unique_size = 1;
     for (int i = 1; i < size; i++) {
       if (numeros[i] != numeros[i - 1]) {
@@ -116,7 +115,7 @@ House* find_coordinates(char* street, int number, char* mapName){
       }
     }
     size = unique_size;
-    
+
     do{
       printf("Introduced number was not found, avaliable ones are the following: ");
       for (int i = 0; i < size; i++) {
@@ -149,111 +148,131 @@ House* find_coordinates(char* street, int number, char* mapName){
 }
 */
 
-
-House* load_houses(char* mapName){
+House *load_houses(char *mapName)
+{
   char filename[SIZE];
   sprintf(filename, "maps/%s/houses.txt", mapName);
-  //printf("Trying to open: %s\n", filename); //prueba
 
-  FILE* file = fopen(filename, "r");
-  if(file == NULL) return NULL;
+  FILE *file = fopen(filename, "r");
+  if (file == NULL)
+    return NULL;
 
   House *head = NULL;
   House temp;
+  int skipped = 0;
 
-  while (fscanf(file, " %127[^,],%d,%lf,%lf", temp.street, &temp.number, &temp.latitude, &temp.longitude) == 4) {
-    
-    House *new = malloc(sizeof(House));
-    
-    *new = temp;
-    new->next = head;
-    head = new;
-
+  char line[512];
+  while (fgets(line, sizeof(line), file))
+  {
+    if (sscanf(line, " %127[^,],%d,%lf,%lf",
+               temp.street, &temp.number,
+               &temp.latitude, &temp.longitude) == 4)
+    {
+      House *new = malloc(sizeof(House));
+      *new = temp;
+      new->next = head;
+      head = new;
+    }
+    else
+    {
+      skipped++;
+    }
   }
 
+  printf("DEBUG houses: skipped %d malformed lines\n", skipped);
   fclose(file);
   return head;
-
 }
 
-
-House* find_house(House *head, char *name, int number){
+House *find_house(House *head, char *name, int number)
+{
   House *current = head;
   int *doors = NULL; // array of possible numbers
   int capacity = 0;
   int size = 0;
   int found = 0;
-  
+
   char nameStripped[SIZE];
   strncpy(nameStripped, name, SIZE - 1);
   nameStripped[SIZE - 1] = '\0';
   normalize(nameStripped);
   get_type_and_strip_prefix(nameStripped);
 
-  while(current != NULL){
+  while (current != NULL)
+  {
     char temp[SIZE];
     strcpy(temp, current->street);
 
     normalize(temp);
     get_type_and_strip_prefix(temp);
-    
-    if(strcmp(temp, nameStripped) == 0){
+
+    if (strcmp(temp, nameStripped) == 0)
+    {
       found = 1;
-      
-      if (size >= capacity) {
+
+      if (size >= capacity)
+      {
         capacity = capacity ? capacity * 2 : 4;
         doors = realloc(doors, capacity * sizeof(int));
       }
       doors[size++] = current->number; // Stores numbers of the addres if the introduced one is not found.
-      if(current->number == number) {
+      if (current->number == number)
+      {
         free(doors);
         return current;
       }
-      
     }
     current = current->next;
   }
-  if (found) {
-    
+  if (found)
+  {
+
     bubble_sort(doors, size);
-    
+
     int unique_size = 1;
-    for (int i = 1; i < size; i++) {
-      if (doors[i] != doors[i - 1]) {
+    for (int i = 1; i < size; i++)
+    {
+      if (doors[i] != doors[i - 1])
+      {
         doors[unique_size++] = doors[i];
       }
     }
     size = unique_size;
-    
+
     printf("Introduced number was not found.\n");
     printf("Available numbers are:\n");
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
       printf("%d ", doors[i]);
     }
     printf("\n");
 
     int new_number;
-    
-    do{
+
+    do
+    {
       printf("Try another number:\n");
       scanf("%d", &new_number);
-      for(int i = 0; i < size; i++){
-        if(new_number == doors[i]){
+      for (int i = 0; i < size; i++)
+      {
+        if (new_number == doors[i])
+        {
           free(doors);
           return find_house(head, name, new_number);
         }
       }
       printf("Invalid number.\n");
 
-    } while(1);
+    } while (1);
   }
 
   char suggestions[MAX_SUGGESTIONS][SIZE];
-  int  distances[MAX_SUGGESTIONS];
-  int  sug_count = 0;
+  int distances[MAX_SUGGESTIONS];
+  int sug_count = 0;
 
   current = head;
-  while (current != NULL) {
+  while (current != NULL)
+  {
     char temp[SIZE];
     strcpy(temp, current->street);
     normalize(temp);
@@ -261,45 +280,63 @@ House* find_house(House *head, char *name, int number){
 
     // Evitar duplicados
     int already = 0;
-    for (int i = 0; i < sug_count; i++) {
-        if (strcmp(suggestions[i], temp) == 0) { already = 1; break; }
+    for (int i = 0; i < sug_count; i++)
+    {
+      if (strcmp(suggestions[i], temp) == 0)
+      {
+        already = 1;
+        break;
+      }
     }
-    if (already) { current = current->next; continue; }
+    if (already)
+    {
+      current = current->next;
+      continue;
+    }
 
     int dist = levenshtein(nameStripped, temp);
-    if (dist <= MAX_DISTANCE) {
-        if (sug_count < MAX_SUGGESTIONS) {
-            strcpy(suggestions[sug_count], temp);
-            distances[sug_count] = dist;
-            sug_count++;
-        } else {
-            // Reemplazar la peor sugerencia si esta es mejor
-            int worst = 0;
-            for (int i = 1; i < MAX_SUGGESTIONS; i++)
-                if (distances[i] > distances[worst]) worst = i;
-            if (dist < distances[worst]) {
-                strcpy(suggestions[worst], temp);
-                distances[worst] = dist;
-            }
+    if (dist <= MAX_DISTANCE)
+    {
+      if (sug_count < MAX_SUGGESTIONS)
+      {
+        strcpy(suggestions[sug_count], temp);
+        distances[sug_count] = dist;
+        sug_count++;
+      }
+      else
+      {
+        // Reemplazar la peor sugerencia si esta es mejor
+        int worst = 0;
+        for (int i = 1; i < MAX_SUGGESTIONS; i++)
+          if (distances[i] > distances[worst])
+            worst = i;
+        if (dist < distances[worst])
+        {
+          strcpy(suggestions[worst], temp);
+          distances[worst] = dist;
         }
+      }
     }
     current = current->next;
   }
 
-  if (sug_count > 0) {
+  if (sug_count > 0)
+  {
     printf("Street not found. Did you mean:\n");
     for (int i = 0; i < sug_count; i++)
-        printf("  %d. %s\n", i + 1, suggestions[i]);
+      printf("  %d. %s\n", i + 1, suggestions[i]);
 
     int choice;
-    do {
-        printf("Choose (1-%d) or 0 to cancel: \n", sug_count);
-        scanf("%d", &choice);
+    do
+    {
+      printf("Choose (1-%d) or 0 to cancel: \n", sug_count);
+      scanf("%d", &choice);
     } while (choice < 0 || choice > sug_count);
 
-    if (choice > 0) {
-        free(doors);
-        return find_house(head, suggestions[choice - 1], number);
+    if (choice > 0)
+    {
+      free(doors);
+      return find_house(head, suggestions[choice - 1], number);
     }
   }
 
@@ -307,19 +344,21 @@ House* find_house(House *head, char *name, int number){
   return NULL;
 }
 
-
-House* house(House *head) {
+House *house(House *head)
+{
 
   char street[SIZE];
   int number;
   House *result = NULL;
 
-  if (head == NULL) {
-      printf("Error loading houses.\n");
-      return NULL;
+  if (head == NULL)
+  {
+    printf("Error loading houses.\n");
+    return NULL;
   }
 
-  do {
+  do
+  {
     printf("Enter street name (e.g. Carrer de Roc Boronat): \n");
     scanf(" %[^\n]", street);
     printf("Enter street number (e.g. 138): \n");
@@ -328,19 +367,22 @@ House* house(House *head) {
 
     result = find_house(head, street, number);
 
-    if (result == NULL) {
+    if (result == NULL)
+    {
       printf("House not found. Try again.\n");
-      }
+    }
 
-  } while(result == NULL);
+  } while (result == NULL);
 
   return result;
 }
 
-void free_houses(House *head) {
-  //printf("DEBUG: Inside free_houses\n");
+void free_houses(House *head)
+{
+  // printf("DEBUG: Inside free_houses\n");
   House *temp;
-  while (head != NULL) {
+  while (head != NULL)
+  {
     temp = head;
     head = head->next;
     free(temp);
--- src/place.c
+++ src/place.c
@@ -24,17 +24,28 @@ Place *load_places(char *mapName)
 
   Place *head = NULL;
   Place temp;
+  int skipped = 0;
 
-  while (fscanf(file, "%128[^,],%128[^,],%128[^,],%lf,%lf", temp.id, temp.name, temp.type, &temp.latitude, &temp.longitude) == 5)
+  char line[512];
+  while (fgets(line, sizeof(line), file))
   {
-    replace_em_dash(temp.name);
-    Place *new = malloc(sizeof(Place));
-
-    *new = temp;
-    new->next = head;
-    head = new;
+    if (sscanf(line, "%127[^,],%127[^,],%127[^,],%lf,%lf",
+               temp.id, temp.name, temp.type,
+               &temp.latitude, &temp.longitude) == 5)
+    {
+      replace_em_dash(temp.name);
+      Place *new = malloc(sizeof(Place));
+      *new = temp;
+      new->next = head;
+      head = new;
+    }
+    else
+    {
+      skipped++;
+    }
   }
 
+  printf("DEBUG places: skipped %d malformed lines\n", skipped);
   fclose(file);
   return head;
 }
@@ -43,8 +54,8 @@ Place *find_place(Place *head, char *name)
 {
   Place *current = head;
   char suggestions[MAX_SUGGESTIONS][SIZE];
-  int  distances[MAX_SUGGESTIONS];
-  int  sug_count = 0;
+  int distances[MAX_SUGGESTIONS];
+  int sug_count = 0;
 
   char nameStripped[SIZE];
   strncpy(nameStripped, name, SIZE - 1);
@@ -59,55 +70,72 @@ Place *find_place(Place *head, char *name)
     normalize(temp);
     get_type_and_strip_prefix(temp);
 
-    //printf("DEBUG comparing: [%s] vs [%s]\n", temp, name);
+    // printf("DEBUG comparing: [%s] vs [%s]\n", temp, name);
     if (strcmp(temp, name) == 0)
       return current;
 
     // Evitar duplicados
     int already = 0;
-    for (int i = 0; i < sug_count; i++) {
-        if (strcmp(suggestions[i], temp) == 0) { already = 1; break; }
+    for (int i = 0; i < sug_count; i++)
+    {
+      if (strcmp(suggestions[i], temp) == 0)
+      {
+        already = 1;
+        break;
+      }
     }
-    if (already) { current = current->next; continue; }
+    if (already)
+    {
+      current = current->next;
+      continue;
+    }
 
     int dist = levenshtein(nameStripped, temp);
-    if (dist <= MAX_DISTANCE) {
-        if (sug_count < MAX_SUGGESTIONS) {
-            strcpy(suggestions[sug_count], temp);
-            distances[sug_count] = dist;
-            sug_count++;
-        } else {
-            // Reemplazar la peor sugerencia si esta es mejor
-            int worst = 0;
-            for (int i = 1; i < MAX_SUGGESTIONS; i++)
-                if (distances[i] > distances[worst]) worst = i;
-            if (dist < distances[worst]) {
-                strcpy(suggestions[worst], temp);
-                distances[worst] = dist;
-            }
+    if (dist <= MAX_DISTANCE)
+    {
+      if (sug_count < MAX_SUGGESTIONS)
+      {
+        strcpy(suggestions[sug_count], temp);
+        distances[sug_count] = dist;
+        sug_count++;
+      }
+      else
+      {
+        // Reemplazar la peor sugerencia si esta es mejor
+        int worst = 0;
+        for (int i = 1; i < MAX_SUGGESTIONS; i++)
+          if (distances[i] > distances[worst])
+            worst = i;
+        if (dist < distances[worst])
+        {
+          strcpy(suggestions[worst], temp);
+          distances[worst] = dist;
         }
+      }
     }
 
     current = current->next;
   }
 
-  if (sug_count > 0) {
+  if (sug_count > 0)
+  {
     printf("Place not found. Did you mean:\n");
     for (int i = 0; i < sug_count; i++)
-        printf("  %d. %s\n", i + 1, suggestions[i]);
+      printf("  %d. %s\n", i + 1, suggestions[i]);
 
     int choice;
-    do {
-        printf("Choose (1-%d) or 0 to cancel: \n", sug_count);
-        scanf("%d", &choice);
+    do
+    {
+      printf("Choose (1-%d) or 0 to cancel: \n", sug_count);
+      scanf("%d", &choice);
     } while (choice < 0 || choice > sug_count);
 
-    if (choice > 0) {
-        return find_place(head, suggestions[choice - 1]);
+    if (choice > 0)
+    {
+      return find_place(head, suggestions[choice - 1]);
     }
   }
   return NULL;
-
 }
 
 Place *place(Place *head)
@@ -139,7 +167,7 @@ Place *place(Place *head)
 
 void free_places(Place *head)
 {
-  //printf("DEBUG: Inside free_places\n");
+  // printf("DEBUG: Inside free_places\n");
 
   Place *temp;
   while (head != NULL)
